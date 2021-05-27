e0fa0099b35cb147cb2142998d731a12d186566e
Den4ick240 <45475878+Den4ick240@users.noreply.github.com>
2021-05-27 19:47:54 +0700

    Add files via upload

--- src/main/java/ru/nsu/ccfit/zhigalov/databases/exchanges/project/Consts.java
+++ src/main/java/ru/nsu/ccfit/zhigalov/databases/exchanges/project/Consts.java
@@ -1,1 +1,27 @@
+package ru.nsu.ccfit.zhigalov.databases.exchanges.project;
 
+public class Consts {
+    public static String getSchemaName() {
+        return "\"18206_ZHIGALOV\".";
+    }
+
+    public static String getLogin() {
+        return "18206_ZHIGALOV";
+    }
+
+    public static String getPassword() {
+        return "Parol12345";
+    }
+
+//    public static String getSchemaName() {
+//        return "\"DA\".";
+//    }
+//
+//    public static String getLogin() {
+//        return "da";
+//    }
+//
+//    public static String getPassword() {
+//        return "sys";
+//    }
+}
